7afdd44d4b9f6b35c33e496cb9a4e57bedf3bbae
JeniferOvejero <116187518+JeniferOvejero@users.noreply.github.com>
2023-06-04 19:26:14 -0300

    Add files via upload

--- tela.py
+++ tela.py
@@ -1,30 +1,14 @@
 import PySimpleGUI as sg
 
-
-class ExemploView:
-
-    def __init__(self):
-        self.__window = None
-        self.init_components()
-
-    def init_components(self):
-        layout = [
-        [sg.Text('Incluir novo cliente')],
-        [sg.Text('Nome', size=(15,1)), sg.InputText('nome', key= "k_nome")],
-        [sg.Text('Endereço', size=(15,1)), sg.InputText('endereço', key="k_endereco")],
-        [sg.Submit(), sg.Cancel()]
-        ]
-        self.__window = sg.Window('Titulo da tela', default_element_size=(40,1)).layout(layout)
-
-    def open(self):
-        button, values = self.__window.read()
-        return button, values
-
-    def close(self):
-        self.__window.close()
-
-    def show_message(self, titulo: str, mensagem: str):
-        sg.popup(titulo, mensagem)
-
-tela = ExemploView()
-tela.open()
+sg.ChangeLookAndFeel('lightblue')
+# Very basic window. Return values as a list
+layout = [
+ [sg.Text('Please enter your Name, Address, Phone')],
+ [sg.Text('Name', size=(15, 1)), sg.InputText('name')],
+ [sg.Text('Address', size=(15, 1)), sg.InputText('address')],
+ [sg.Text('Phone', size=(15, 1)), sg.InputText('phone')],
+ [sg.Submit(), sg.Cancel()]
+ ]
+window = sg.Window('Simple data entry window').Layout(layout)
+button, values = window.Read()
+print(button, values[0], values[1], values[2])
